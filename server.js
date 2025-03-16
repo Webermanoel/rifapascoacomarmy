b120f730872aacce8d8133eb67f6d01dfc173d51
Webermanoel <webermanoel22@gmail.com>
2025-03-16 18:09:28 -0400

    rifa bts

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,      
-  user: process.env.MYSQLUSER,      
-  password: process.env.MYSQLPASSWORD,  
-  database: process.env.MYSQLDATABASE,  
-  port: process.env.MYSQLPORT || 3306
+  host: process.env.DB_HOST, 
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME, 
+  port: process.env.DB_PORT || 3306,
 });
 
 db.connect((err) => {
