37c2df508d0ab5d0c917e52fa8f7b20669b563dc
Webermanoel <webermanoel22@gmail.com>
2025-03-16 21:56:14 -0400

    rifa bts

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 5432,
-  ssl: { rejectUnauthorized: false }  
+  ssl: { rejectUnauthorized: false } 
 });
 
 pool.connect((err) => {
